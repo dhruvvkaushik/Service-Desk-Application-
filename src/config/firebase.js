90fb29bc3aaf5f7f82b9d33175cac8999d84dc35
Dhruv Kaushik <dhruv.kaushik0009@gmail.com>
2025-07-10 18:37:34 +0530

    fix

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -6,13 +6,13 @@ import { getAnalytics } from 'firebase/analytics';
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyA51tsxhe5XdV-LR3D6Z-IAA14nbXXTFRI",
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "service-desk-78255.firebaseapp.com",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "service-desk-78255",
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "service-desk-78255.firebasestorage.app",
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "505237250317",
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:505237250317:web:ffab2590dd35561c83d044",
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-2KSKX4RCXE"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY ,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN ,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID ,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID 
 };
 
 // Initialize Firebase
